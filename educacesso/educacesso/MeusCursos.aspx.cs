47d0fd272bc51def00848bb49c2e4a5f784608af
thiago901a <thiago19a@gmail.com>
2015-04-05 00:16:36 -0300

    Meus Cursos

---
+++ educacesso/educacesso/MeusCursos.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace educacesso
+{
+    public partial class MeusCursos : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                Carregar_Cursos();
+
+            }
+        }
+
+        public static string CurrentUserName
+        {
+            get
+            {
+                string userName = "";
+                if (HttpContext.Current.User.Identity.IsAuthenticated)
+                    userName = HttpContext.Current.User.Identity.Name;
+                return userName;
+            }
+        }
+
+        void Carregar_Cursos()
+        {
+            SqlCommand cmd = new SqlCommand("SELECT * FROM tblCurso WHERE COD_USUARIO=@COD", new ConnectionFactory().getConnection());
+            cmd.Parameters.AddWithValue("@COD", new CursoAddDAO().BuscarUsuario());
+
+            gvw_cursos.DataSource = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+            gvw_cursos.DataBind();
+
+        }
+    }
+}
---
+++ educacesso/educacesso/dao/CursoAddDAO.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace educacesso
+{
+    public class CursoAddDAO
+    {
+        public string Codigo { get; set; }
+        public string Titulo { get; set; }
+        public string Resumo { get; set; }
+        public string Conteudo { get; set; }
+        public int Contador { get; set; }
+        public int Gostei { get; set; }
+
+        public static string CurrentUserName
+        {
+            get
+            {
+                string userName = "";
+                if (HttpContext.Current.User.Identity.IsAuthenticated)
+                    userName = HttpContext.Current.User.Identity.Name;
+                return userName;
+            }
+        }
+
+        public void CadastraCurso(String _titulo, String _resumo, String _conteudo)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand("INSERT INTO tblCurso(TITULO_CURSO, RESUMO_CURSO, CONTEUDO_CURSO, COD_USUARIO, dataCadastro) VALUES(@TITULO, @RESUMO, @CONTEUDO, @IDUSUARIO, @DATACADASTRO)", new ConnectionFactory().getConnection());
+                cmd.Parameters.AddWithValue("@TITULO", _titulo);
+                cmd.Parameters.AddWithValue("@RESUMO", _resumo);
+                cmd.Parameters.AddWithValue("@CONTEUDO", _conteudo);
+                cmd.Parameters.AddWithValue("@IDUSUARIO", BuscarUsuario());
+                cmd.Parameters.AddWithValue("@DATACADASTRO", DateTime.Now);
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception erx)
+            {//"Ocorreu um erro inesperado";//erx.ToString()
+                throw new Exception(erx.ToString());
+            }
+
+        }
+
+
+        public String BuscarUsuario()
+        {
+            SqlCommand cmd = new SqlCommand("SELECT COD_USUARIO FROM USUARIO WHERE USUARIO_ID=@NOME", new ConnectionFactory().getConnection());
+            cmd.Parameters.AddWithValue("@NOME", CurrentUserName);
+
+            SqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);        
+            while (dr.Read())
+            {
+                Codigo = dr["COD_USUARIO"].ToString();
+               
+            }
+            return Codigo;
+
+        }
+    }
+}
